0464878c722b42c19e9fb36411cfcc0df79dd2f6
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2025-09-20 22:03:49 -0300

    todo con tailwind + editprofile

---
+++ frontend/src/pages/AppLogin.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import bannerLogin from "./assets/banner-login.png";
+
+function AppLogin() {
+  return (
+    <div className="relative min-h-screen bg-white flex flex-col items-center justify-start py-12">
+      {/* Fondo */}
+      <img
+        src={bannerLogin}
+        alt="Fondo"
+        className="fixed inset-0 w-full h-full object-cover z-0"
+      />
+
+      {/* Título principal */}
+      <h1 className="relative z-10 text-white text-center font-extrabold text-[clamp(2rem,8vw,8rem)] drop-shadow-[0_8px_9px_rgba(0,0,0,0.5)] mb-12">
+        MedManager
+      </h1>
+
+      {/* Contenedor de login */}
+      <div className="relative z-10 w-[90%] max-w-[50.25rem] bg-white rounded-lg shadow-md p-10 flex flex-col gap-6 border-white border-30">
+        <h2 className="text-3xl font-semibold text-black text-center mb-6">
+          Inicio de sesión
+        </h2>
+
+        <div className="flex flex-col gap-4">
+          <div>
+            <label className="block text-black font-normal mb-2">
+              Correo Electrónico:
+            </label>
+            <input
+              type="email"
+              placeholder="Ingresa tu correo electrónico"
+              className="w-full bg-[#95B5C4] rounded-md border-none p-4 text-black"
+            />
+          </div>
+
+          <div>
+            <label className="block text-black font-normal mb-2">
+              Contraseña:
+            </label>
+            <input
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              className="w-full bg-[#95B5C4] rounded-md border-none p-4 text-black"
+            />
+          </div>
+        </div>
+
+        <button className="w-3/5 h-14 bg-[#00AAFF] text-white text-xl font-semibold rounded-md shadow-md hover:brightness-110 transition self-center mt-4">
+          Iniciar sesión
+        </button>
+      </div>
+
+      {/* Registro debajo del contenedor */}
+      <div className="relative z-10 mt-10 text-center text-black text-base">
+        <span>¿No tienes una cuenta? </span>
+        <span className="text-[#76F1FF] font-bold cursor-pointer">
+          Regístrate aquí.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default AppLogin;
--- login/src/main.jsx
+++ login/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppRegistro.jsx'
+import EditProfile from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <EditProfile />
   </StrictMode>,
 )
