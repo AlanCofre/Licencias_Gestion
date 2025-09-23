1fa5e0b979ec89c4a6382168ce9a78a2d94141c8
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2025-09-23 00:23:46 -0300

    conexión login/registro + corrección errores

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
+import AppLogin from "./pages/AppLogin";
+import AppRegistro from "./pages/AppRegistro";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-blue-100">
       <Navbar />
-      <Dashboard />
+      <AppLogin />
+      <AppRegistro />
       <div className="flex-grow" />
       <Footer />
     </div>
