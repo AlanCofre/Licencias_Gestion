96f0bb126786bc7a24bba21431be5fae7f33e2b4
cofranco <chacotaco06@gmail.com>
2025-09-27 05:46:01 -0300

    correccion de errores

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,11 @@ import notificationRouter from './notification/notificacion.js';
 import licenciaRoutes from './routes/licencias.routes.js';
 import archivoRoutes from './routes/archivo.routes.js';
 import usuarioRoutes from './routes/usuario.route.js';
+import licenciaArchivoRouter from './routes/licencia-archivo.js';
+
+
+
+
 
 // Pool de mysql2/promise exportado desde ./db/db.js
 // (si tu módulo exporta `module.exports = pool`, esto sigue funcionando como default en ESM)
@@ -44,6 +49,8 @@ app.use('/licencias', detailsRouter);
 app.use('/archivos', insertRouter);
 app.use('/notificaciones', notificationRouter);
 app.use('/api/archivos', archivoRoutes);
+app.use('/api', licenciaArchivoRouter);
+
 
 /* === 404 (no encontrado) === */
 app.use((req, res) => {
