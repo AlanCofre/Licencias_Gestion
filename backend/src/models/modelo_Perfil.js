3a6a170048fe38d2c0cde2666cc109c1bc15fbd4
vhictor05 <victor.gonzalez2024@alu.uct.cl>
2025-09-28 01:36:28 -0300

    correcion errores
    correccion de errores minimos en modelos

--- backend/src/models/modelo_Perfil.js
+++ backend/src/models/modelo_Perfil.js
@@ -4,16 +4,15 @@ import sequelize from '../../db/sequelize.js';
 const Perfil = sequelize.define('Perfil', {
   id_perfil:   { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   id_usuario:  { type: DataTypes.INTEGER, allowNull: false, unique: true },
-  correo_alt:  { type: DataTypes.STRING(255), allowNull: true, validate: { isEmail: { msg: 'Correo alternativo inválido' } } },
-  telefono:    { type: DataTypes.STRING(20),  allowNull: true, validate: { is: { args: [/^[+()\-\s\d]{6,20}$/], msg: 'Teléfono inválido' } } },
+  email_alt:  { type: DataTypes.STRING(255), allowNull: true, validate: { isEmail: { msg: 'Correo alternativo inválido' } } },
+  numero_telef:    { type: DataTypes.STRING(20),  allowNull: true, validate: { is: { args: [/^[+()\-\s\d]{6,20}$/], msg: 'Teléfono inválido' } } },
   direccion:   { type: DataTypes.STRING(255), allowNull: true },
   foto_url:    { type: DataTypes.TEXT,        allowNull: true, validate: { isUrl: { msg: 'URL de foto inválida' } } }
 }, {
   modelName: 'Perfil',
   tableName: 'perfil',              // <-- exactamente como en MySQL
-  timestamps: true,
-  createdAt: 'creado_en',           // <-- mapea tus timestamps
-  updatedAt: 'actualizado_en'
+  timestamps: false,
+
 });
 
 export default Perfil;
